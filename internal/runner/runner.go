37d35a3d090567fdcf2286ae67eff591c77c5a1d
Anuraag Agrawal <anuraaga@gmail.com>
2025-10-11 18:55:22 +0900

    Check for empty env string before setting

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -36,6 +36,10 @@ func Run(name string, args []string, wasm []byte, stdin io.Reader, stdout io.Wri
 						WithArgs(args...).
 						WithFSConfig(wazero.NewFSConfig().(sysfs.FSConfig).WithSysFSMount(root, "/"))
 	for _, env := range os.Environ() {
+		if env == "" {
+			// Work around possible bug in Go on Windows returning empty env variables.
+			continue
+		}
 		k, v, _ := strings.Cut(env, "=")
 		if k == "PWD" {
 			// Causes Haskell runtime to try chdir which isn't supported in wasip1
